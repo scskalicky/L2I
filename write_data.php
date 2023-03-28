c19cc4bfc6762d3d4c1b1cf0f5bfd29596eaf97b
Stephen Skalicky <scskalicky@gmail.com>
2023-03-28 10:44:55 -0600

    Revert "sanitise php output"
    This reverts commit 7fcc9e83aa10ca0c092df8d3df530a2399896676.

--- write_data.php
+++ write_data.php
@@ -11,16 +11,16 @@ $subject = $_POST['subject'];
 $email = $_POST['email'];
 
 // combine subject and email into one line
-$line = array(htmlspecialchars($subject), htmlspecialchars($email));
+$line = array($subject, $email);
 
 // create filename (folder should already be created)
 //$name = "data/" . $subject . "/" . $subject . "_results.csv"; 
 
 // don't create a separat folder for each person 
-$name = "data/" . htmlspecialchars($subject) . "_results.csv"; 
+$name = "data/" . $subject . "_results.csv"; 
 
 // write the file to disk
-file_put_contents($name, htmlspecialchars($data));
+file_put_contents($name, $data);
 
 
 // open master csv file
