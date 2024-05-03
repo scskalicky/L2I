6e37047adfccd573b15d18ba4c4e05c24046293b
Stephen Skalicky <scskalicky@gmail.com>
2024-05-03 20:19:33 +1200

    Update pull_ft.php

--- pull_ft.php
+++ pull_ft.php
@@ -1,5 +1,9 @@
 <?php
 
-if ( $_POST['payload'] ) {
+// if ( $_POST['payload'] ) {
+//     shell_exec('git pull'); 
+// }
+
+if ( $_SERVER['HTTP_X_GITHUB_EVENT'] === 'push' ) {
     shell_exec('git pull'); 
 }
